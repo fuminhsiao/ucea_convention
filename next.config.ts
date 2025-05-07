cadac998ae7e6c8c63f645b0c29c725e673b490e
unknown <ifumin.hs@gmail.com>
2025-05-06 20:57:51 -0400

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // 🔧 啟用靜態匯出
-  basePath: "/ucea-convention", // ⛳️ 替換為你的 GitHub repo 名稱
+  basePath: "", // ⛳️ 替換為你的 GitHub repo 名稱
   trailingSlash: true, // 📁 確保每頁輸出為 index.html 結構
   images: {
     unoptimized: true, // ❗ next/image 在 export 模式下必須設定
